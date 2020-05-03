f0bf0293a41679aa57531a51774c73cdd9f746a3
bokdoll <winnerhj1109@naver.com>
2020-05-03 20:47:25 +0900

    Updates

---
+++ precache-manifest.e6e58e195c5573fc643f947da5201be6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fbf97974e512519e29dd436b0475178",
+    "url": "/movie_app_nomad/index.html"
+  },
+  {
+    "revision": "f9fbbc93969b1a8667c4",
+    "url": "/movie_app_nomad/static/css/main.f31bb84f.chunk.css"
+  },
+  {
+    "revision": "fae7e38643050a681f47",
+    "url": "/movie_app_nomad/static/js/2.c3e696b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_nomad/static/js/2.c3e696b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9fbbc93969b1a8667c4",
+    "url": "/movie_app_nomad/static/js/main.852646ca.chunk.js"
+  },
+  {
+    "revision": "5006c551b30565a5e73f",
+    "url": "/movie_app_nomad/static/js/runtime-main.d59d02a1.js"
+  }
+]);
